18615c67fd6a9b7f51a6fb8ec5d8e576e294afbb
ThomasL642 <ThomasLawrence642@gmail.com>
2024-10-28 23:23:19 -0400

    Added Dice

---
+++ Lab 6/Dice.js
@@ -0,0 +1,31 @@
+class Roll {
+    constructor(min = 1, max = 6) {
+        this.min = min;
+        this.max = max;
+        this.result = this.roll();
+    }
+
+    roll() {
+        this.result = Math.floor(Math.random() * this.max) + this.min;
+        return this.result;
+    }
+}
+
+class RollHistory {
+    constructor() {
+        this.history = [];
+    }
+
+    addRoll(roll) {
+        this.history.push(roll);
+    }
+
+    getHistory() {
+        return this.history;
+    }
+
+    clearHistory() {
+        this.history = [];
+    }
+
+}
